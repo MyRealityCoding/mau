49669f26cb73e69015a4dc0344d7a601bf0e0976
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2014-08-29 18:25:18 +0200

    Converted game to object context

--- src/game.js
+++ src/game.js
@@ -1,50 +1,65 @@
-function getCanvas() {
-	return $('canvas');
-}
+var MauGame = {
 
-function getContext() {
-	canvasElement = getCanvas();
-	return canvasElement.get(0).getContext("2d");
-}
+	init: function() {
 
-function gameWidth() {
-	return getCanvas().width();
-}
+		this.entities = [];
 
-function gameHeight() {
-	return getCanvas().height();
-}
+		var w = $(window).width();
+		var h = $(window).height();
+		var canvasElement = $("<canvas width='" + w + 
+		                      "' height='" + h + "'></canvas>");
+		$('body').html(canvasElement);	
+	},	
+	canvas: function(){
+		return $('canvas');
+	},
+	context: function() {
+		return this.canvas().get(0).getContext("2d");
+	},
+	width: function() {
+		return this.canvas().width();
+	},
+	height: function() {
+		return this.canvas().height();
+	},
+	update: function(dt) {
+		for (var e in this.entities) {
+			e.update(dt);
+		}
+	},
+	draw: function() {
+		this.context().clearRect(0, 0, this.width(), this.height());
+		for (var e in this.entities) {
+			e.draw(this.context());
+		}
+	},
+	resize: function(w,h) {
+		this.canvas().attr('width', w);
+		this.canvas().attr('height', h);
+		for (var e in this.entities) {
+			e.resize(w,h);
+		}
+	},
+	addEntity: function(entity) {
+		this.entities.push(entity);
+	}
+};
 
-function init() {
-	var w = $(window).width();
-	var h = $(window).height();
-	var canvasElement = $("<canvas width='" + w + 
-	                      "' height='" + h + "'></canvas>");
-	$('body').html(canvasElement);	
-}
-
-function update(dt) {
-
-}
-
-function draw() {
-	  getContext().clearRect(0, 0, gameWidth(), gameHeight());
-}
+var game = Object.create(MauGame);
 
 $(document).ready(function() {
 
-	init();
+		game.init();
 
-	var FPS = 30;
+		var FPS = 30;
 
-	setInterval(function() {
-	  update(1);
-	  draw();
-	}, 1000 / FPS);
-});
+		setInterval(function() {
+		  game.update(1);
+		  game.draw();
+		}, 1000 / FPS);
+	});
 
 // Always fit to window size
 $(window).resize(function() {
-	getCanvas().attr('width', $(window).width());
-	getCanvas().attr('height', $(window).height());
+	game.resize($(window).width(), $(window).height());
 });
